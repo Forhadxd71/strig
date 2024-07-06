68e84ed5e8797efd85a892cfd074f1bea5a78b89
Forhad71 <112680572+Forhadxd71@users.noreply.github.com>
2024-07-06 12:28:55 +0600

    Create s.py

---
+++ s.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, enums
+
+api_id = input("Enter Your API ID: ")
+
+api_hash = input("Enter Your API HASH: ")
+
+phone_number = input("Enter Your Phone Number: ")
+
+user = Client("nom", api_id, api_hash, phone_number=phone_number, in_memory=True)
+
+user.start()
+
+session_string = user.export_session_string()
+
+print(f"Your Session String is:\n\n{session_string}")
+
+user.send_message('me', f'**SESSION STRING FROM USERBOT:**\n\n`{session_string}`', parse_mode=enums.ParseMode.MARKDOWN)
+
+print('\n\nAlso this string is saved in your SAVED MESSAGES section')
